ce8bc34c8c17183b2c06bc184ed1a499debe4504
faust45 <faust451@gmail.com>
2010-11-06 21:49:50 +0500

    Add print list recursive

--- app.erl
+++ app.erl
@@ -3,12 +3,16 @@
 -export([start/0]).
 
 start() ->
-  print(),
-prin().
+  print_list([1,2,3,4]).
+
 print() ->
   io:format("Wait for client...~n").
 
-  
-
 prin() ->
   io:format("Wait for...~n").
+
+print_list([]) -> [];
+    
+print_list([FirstEl | List]) ->
+  io:format("~p~n", [FirstEl]),
+  print_list(List).
